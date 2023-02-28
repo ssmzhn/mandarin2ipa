d537ae743a366e9ff0a19223f63aa45fb6d5461b
ssmzhn <ssmzhn@163.com>
2023-02-28 22:43:22 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,38 @@
+import json
+from pypinyin import pinyin, Style
+import re
+tone_list = ["˥","˧˥","˨˩˦","˥˩",""]
+ipa_table = json.load(open('ipa.json'))
+select_table = ['拼音声调','2075北京话音系','UntPhesoca宽','UntPhesoca严','Standard Chinese (Beijing)','Standard Chinese (Beijing)严','胡裕树《现代汉语》','黄伯荣、廖序东《现代汉语》','钱乃荣《现代汉语》','吴宗济','赵元任《汉语口语语法》','《汉语方音字汇》']
+def septone(syllable:str) -> tuple:
+    tone = syllable[-1]
+    return (syllable[:-1],int(syllable[-1]))
+
+def mandarin2ipa(s:str,mode=2):
+    if mode == 0:
+        return pinyin(s,heteronym=True)
+    converted = []
+    pinyin_list = pinyin(s,heteronym=True,style=Style.TONE3,neutral_tone_with_five=True)
+    for x in pinyin_list:
+        if not re.match('^[A-Za-z]+[1-5]$',x[0]):
+            converted.append(x)
+            continue
+        char_list = []
+        for y in x:
+            sep = septone(y)
+            #print(sep)
+            char_list.append(
+                ipa_table[sep[0]][select_table[mode]]+tone_list[sep[1]-1]
+            )
+        converted.append(char_list)
+    return converted
+def converted2string(word,converted_table)->str:
+    output = ''
+    for x,y in enumerate(converted_table):
+        output += word[x]
+        if word[x]==y[0]:
+            continue
+        output+=' ('
+        output += ' '.join(y)
+        output +=') '
+    return output
